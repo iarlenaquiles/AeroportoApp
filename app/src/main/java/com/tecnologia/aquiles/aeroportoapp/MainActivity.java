245bd50e84908a79c8baedd69f1c225409bbce07
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-10-19 15:29:08 -0300

    Definindo os ids dos componentes e iniciando na main

--- app/src/main/java/com/tecnologia/aquiles/aeroportoapp/MainActivity.java
+++ app/src/main/java/com/tecnologia/aquiles/aeroportoapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.tecnologia.aquiles.aeroportoapp;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
+
+    ImageView image;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.minhaImagem);
+        texto = findViewById(R.id.meuTexto);
+        texto.setText("Toque para continuar.");
     }
 }
