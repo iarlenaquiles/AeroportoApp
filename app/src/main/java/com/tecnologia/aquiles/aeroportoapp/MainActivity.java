6f150c66fd8c428a8b8aa5b6348811d5f81eb458
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-10-19 16:20:16 -0300

    Usando a animation listener

--- app/src/main/java/com/tecnologia/aquiles/aeroportoapp/MainActivity.java
+++ app/src/main/java/com/tecnologia/aquiles/aeroportoapp/MainActivity.java
@@ -24,12 +24,49 @@ public class MainActivity extends Activity {
 
         image = findViewById(R.id.minhaImagem);
         texto = findViewById(R.id.meuTexto);
+
         texto.setText("Toque para continuar.");
+        image.setVisibility(View.INVISIBLE);
+
         aparece = new AlphaAnimation(0,1);
         some = new AlphaAnimation(1,0);
 
         some.setDuration(500);
         aparece.setDuration(500);
+
+        aparece.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                image.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                image.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        some.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                image.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                image.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
     public void clicouTela(View view) {
@@ -40,6 +77,7 @@ public class MainActivity extends Activity {
             texto.setText("Siga para Direita");
             image.setScaleX(-1f);
         }
-        image.startAnimation(some);
+
+        image.startAnimation(aparece);
     }
 }
